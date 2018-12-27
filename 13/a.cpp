ace1885ca3ff137c69be03580f81d820a0b30e9e
unipota <woz.eon@gmail.com>
2018-12-28 01:11:35 +0900

    13

---
+++ 13/a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
+#define r(i,n) for(int i=0;i<n;i++)
+#define R(i,c,n) for(int i=c;i<n;i++)
+#define DEBUG(x) cerr << #x << ": " << x << endl;
+#define DUMP(a) REP(d,a.size()){cout<<a[d];if(d!=a.size()-1)cout<<" ";else cout<<endl;}
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v)  sort(ALL(v));v.erase(unique(ALL(v)),v.end());
+#define pb push_back
+using namespace std;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+const double PI = 3.14159265358979323846;
+
+int main(){
+  ios::sync_with_stdio(false);
+	cin.tie(0);
+  char a;
+  cin >> a;
+  cout << a - 64 << endl;
+  return 0;
+}
