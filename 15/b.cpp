b7663587ecd42b04b5f2c8405edc98373c4458d5
unipota <unipota@monolith.local>
2019-01-02 21:51:30 +0900

    ABC15,AB

---
+++ 15/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define r(i,n) for(int i=0;i<n;i++)
+#define R(i,c,n) for(int i=c;i<n;i++)
+#define DEBUG(x) cerr << #x << ": " << x << endl;
+#define DUMP(a) REP(d,a.size()){cout<<a[d];if(d!=a.size()-1)cout<<" ";else cout<<endl;}
+#define ALL(v) v.begin(),v.end()
+#define UNIQUE(v)  sort(ALL(v));v.erase(unique(ALL(v)),v.end());
+#define pb push_back
+using namespace std;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+const double PI = 3.14159265358979323846;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  double a[n];
+  r(i,n) cin >> a[i];
+  double sum = .0;
+  int count = 0;
+  r(i,n) {
+    sum += a[i]!=0?a[i]:0;
+    count += a[i]!=0?1:0;
+  }
+  cout << ceil(sum/count) << endl;
+  return 0;
+}
